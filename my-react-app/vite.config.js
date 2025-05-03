a6c95acd1bb45f2bd40c8d47bc7584859b08ceb4
Swapnil-6062 <swapnilnagawade84@gmail.com>
2025-05-03 13:51:00 +0530

    First push

---
+++ my-react-app/vite.config.js
@@ -0,0 +1,12 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Allow external access (this allows access from your EC2 public IP)
+    port: 5173
+  }
+})
